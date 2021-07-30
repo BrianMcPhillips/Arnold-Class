074d13b9d2c8c7a99e924ab5d4b0e9552c511415
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-30 00:27:56 -0700

    Data coming back from API and stored in state

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,6 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import request from 'superagent';
 
 export default class SearchPage extends Component {
+  state = {
+    characterState: []
+  }
+  componentDidMount = async() => {
+    const data = await request
+    .get(`https://hey-arnold-api.herokuapp.com/api/v1/characters`)
+    .accept('application/json');
+    
+    this.setState({ characterState: data.body});
+  }
   render() {
     return (
       <div>
